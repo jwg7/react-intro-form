4bc57946754887f9a23fa22265381f9b50f1af22
JG <105654452+jwg7@users.noreply.github.com>
2022-10-15 18:28:08 -0500

    part 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import { useState } from "react";
 import "./App.css";
 
-///////////////////////////////
+///////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+const MoviesDisplay = (props) => {
+  return (
+    <div>
+      {props.movieList.map((movie, index) => {
+        return <MovieItem movie={movie} key={index} />;
+      })}
+    </div>
+  );
+};
+
+////////////////////////////////////////////////////
+///////////////////////////////////////////////////
+
+const MovieItem = (props) => {
+  return (
+    <div>
+      <h2>Title: {props.movie.title}</h2>
+      <p>Director: {props.movie.director}</p>
+      <p>Actors: {props.movie.actors}</p>
+      <p>Plot: {props.movie.plot}</p>
+      <p>Year: {props.movie.year}</p>
+      <p>IMDB Rating: {props.movie.imdbRating}</p>
+    </div>
+  );
+};
+
+/////////////////////////////////////////////////////
+////////////////////////////////////////////////////
 
 function MovieForm(props) {
   const [title, setTitle] = useState("");
@@ -10,7 +40,6 @@ function MovieForm(props) {
   const [plot, setPlot] = useState("");
   const [year, setYear] = useState(0);
   const [imdbRating, setImdbRating] = useState(0);
-  console.log(title);
   return (
     <div>
       <h1>Movie Form</h1>
@@ -62,26 +91,56 @@ function MovieForm(props) {
           setImdbRating(event.target.value);
         }}
       />
-      <p>Current Title: {title}</p>
+      <br />
+      <button
+        className="add-movie"
+        onClick={() => {
+          console.log("clicked");
+          const newMovie = {
+            title: title,
+            director: director,
+            actors: actors,
+            plot: plot,
+            year: year,
+            imdbRating: imdbRating,
+          };
+          props.handleAddMovie(newMovie);
+        }}
+      >
+        Add Movie
+      </button>
+      {/* Commenting out lines 113 - 118 due to space. Only completed due to Part 1. Part 2 looks much cleaner with this removed. */}
+      {/* <p>Current Title: {title}</p>
       <p>Current Director: {director}</p>
       <p>Current Actors: {actors}</p>
       <p>Current Plot: {plot}</p>
       <p>Current Year: {year}</p>
-      <p>Current imdb Rating: {imdbRating}</p>
+      <p>Current imdb Rating: {imdbRating}</p> */}
     </div>
   );
 }
 
-//////////////////////////////////////////
+//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  console.log(movieList);
+  const handleAddMovie = (newMovie) => {
+    const movieListCopy = [...movieList];
+    movieListCopy.push(newMovie);
+    setMovieList(movieListCopy);
+  };
+
   return (
     <div className="App App-header">
-      <MovieForm />
+      <MovieForm handleAddMovie={handleAddMovie} />
+      <MoviesDisplay movieList={movieList} />
     </div>
   );
 }
 
-////////////////////////////////////////
+////////////////////////////////////////////////////
+///////////////////////////////////////////////////
 
 export default App;
